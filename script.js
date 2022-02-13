06395157dfd038e3a4051e6b4a413c3036c20837
whyyor <whyyor@gmail.com>
2022-02-13 16:38:02 +0530

    initialize project

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position); //this is the case when it succeeds
+    }, function () {
+        alert('Could not get your position') //in case if it fails
+    })
+}
