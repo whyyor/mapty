820070f258fcc56ac079da68b4adf35a0dbc6772
whyyor <whyyor@gmail.com>
2022-02-13 16:55:45 +0530

    add leaflet to access map

--- script.js
+++ script.js
@@ -21,6 +21,17 @@ if (navigator.geolocation) {
             longitude
         } = position.coords;
         console.log(latitude, longitude)
+
+        const map = L.map('map').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+
     }, function () {
         alert('Could not get your position') //in case if it fails
     })
