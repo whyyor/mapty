81be53a93b95fff29c6efec5e9a7f40aa9a7f6c6
whyyor <whyyor@gmail.com>
2022-02-15 14:07:53 +0530

    update marker and workout section

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
         //in case if it succeeds
@@ -22,17 +24,54 @@ if (navigator.geolocation) {
         } = position.coords;
         console.log(latitude, longitude)
 
-        const map = L.map('map').setView([51.505, -0.09], 13);
+        const coords = [latitude, longitude];
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([51.5, -0.09]).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
+        //Handling click on map
+        map.on('click', function (mapE) { //we listen to click on map
+            mapEvent = mapE;
+            form.classList.remove('hidden');
+            inputDistance.focus();
+
+        })
 
     }, function () {
         alert('Could not get your position') //in case if it fails
     })
 }
+
+form.addEventListener('submit', function (e) {
+    console.log(mapEvent)
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+
+    // Display the marker
+    const {
+        lat,
+        lng
+    } = mapEvent.latlng; //we destructure latlng var from mapEvent
+    L.marker([lat, lng]).addTo(map) //we show a marker at clicked latlng
+        .bindPopup(L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false, //this one keeps popup open when
+            //other marker is selected
+            closeOnClick: false,
+            className: 'running-popup',
+        })) //this is the message shown
+        .setPopupContent('workout')
+        .openPopup();
+})
+
+inputType.addEventListener('change', function () {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+})
